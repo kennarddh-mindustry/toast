4f6e663ac6b4596096433abe23b8c988d3ee7ce0
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-03 13:59:44 +0700

    feat(core): Move loaded message to after register handler

--- toast-core/src/main/kotlin/kennarddh/toast/core/Toast.kt
+++ toast-core/src/main/kotlin/kennarddh/toast/core/Toast.kt
@@ -4,13 +4,12 @@ import arc.util.Log
 import kennarddh.genesis.core.Genesis
 import kennarddh.genesis.core.commons.AbstractPlugin
 import kennarddh.toast.core.handlers.UserXPHandler
-import mindustry.gen.Call
 
 @SuppressWarnings("unused")
 class Toast : AbstractPlugin() {
     override fun init() {
-        Log.info("[Toast] Loaded")
-
         Genesis.addHandler(UserXPHandler())
+
+        Log.info("[Toast] Loaded")
     }
 }
