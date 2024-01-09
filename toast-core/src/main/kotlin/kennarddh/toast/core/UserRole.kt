71b55334f878664ca149e7c495418a1b7d2620db
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-09 12:19:19 +0700

    feat: Role enum for user

---
+++ toast-core/src/main/kotlin/kennarddh/toast/core/UserRole.kt
@@ -0,0 +1,8 @@
+package kennarddh.toast.core
+
+enum class UserRole(val id: String) {
+    Owner("Owner"),
+    CoOwner("CoOwner"),
+    Admin("Admin"),
+    Mod("Mod"),
+}
--- toast-core/src/main/kotlin/kennarddh/toast/core/database/tables/Users.kt
+++ toast-core/src/main/kotlin/kennarddh/toast/core/database/tables/Users.kt
@@ -1,9 +1,16 @@
 package kennarddh.toast.core.database.tables
 
+import kennarddh.toast.core.UserRole
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 
 object Users : IntIdTable() {
     val username: Column<String> = varchar("username", 255)
     val password: Column<String> = varchar("password", 255)
+    val role =
+        customEnumeration(
+            "role",
+            "ENUM(${UserRole.entries.joinToString(", ") { "'${it.id}'" }}",
+            { value -> UserRole.entries.find { it.id == value } as UserRole },
+            { it.id })
 }
