fb80bc08d1036532b026a32638a31fc16bd10f36
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-15 00:03:54 +0700

    fix: Make mindustry user's user id nullable

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/MindustryUser.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/MindustryUser.kt
@@ -3,6 +3,6 @@ package com.github.kennarddh.mindustry.toast.core.commons.database.tables
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object MindustryUser : IntIdTable() {
-    val userID = reference("userID", Users)
+    val userID = reference("userID", Users).nullable()
     val mindustryUUID = varchar("mindustryUUID", 255)
 }
