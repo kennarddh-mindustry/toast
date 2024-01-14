36a24b08531a1b18c6dea4a71ca22f5ad4f8d2b2
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-15 00:12:18 +0700

    feat: Remove user role id from enum

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/UserRole.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/UserRole.kt
@@ -1,9 +1,9 @@
 package com.github.kennarddh.mindustry.toast.core.commons
 
-enum class UserRole(val id: String) {
-    Owner("Owner"),
-    CoOwner("CoOwner"),
-    Admin("Admin"),
-    Mod("Mod"),
-    Player("Player"),
+enum class UserRole() {
+    Owner,
+    CoOwner,
+    Admin,
+    Mod,
+    Player,
 }
