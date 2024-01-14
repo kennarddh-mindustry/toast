b1951c5f4c0b26060db0170f6453464856223d41
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-14 23:26:44 +0700

    feat: IP address extensions for int and string

---
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/IPAddressExtensions.kt
@@ -0,0 +1,22 @@
+package com.github.kennarddh.mindustry.toast.core.commons
+
+class InvalidIPStringException(override val message: String) : RuntimeException(message)
+
+fun String.packIP(): Int {
+    try {
+        val intArray = this.split(".").map { it.toInt() }
+
+        return intArray.get(0) shl 24 or (intArray.get(1) shl 16) or (intArray.get(2) shl 8) or intArray.get(3)
+    } catch (error: NumberFormatException) {
+        throw InvalidIPStringException("IP String is not a valid ip")
+    }
+}
+
+fun Int.unpackIP(): String {
+    return intArrayOf(
+        this shr 24 and 0xff,
+        this shr 16 and 0xff,
+        this shr 8 and 0xff,
+        this and 0xff
+    ).joinToString(".")
+}
