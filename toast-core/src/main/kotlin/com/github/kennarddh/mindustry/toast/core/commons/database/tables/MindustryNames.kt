7177aafa47a12d13b092201098c77edbfe41e2da
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-14 23:35:08 +0700

    fix: Mindustry names table wrong column type

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/MindustryNames.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/MindustryNames.kt
@@ -3,6 +3,6 @@ package com.github.kennarddh.mindustry.toast.core.commons.database.tables
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object MindustryNames : IntIdTable() {
-    val name = integer("name").uniqueIndex()
-    val strippedName = integer("strippedName")
+    val name = varchar("name", 50).uniqueIndex()
+    val strippedName = varchar("strippedName", 50)
 }
