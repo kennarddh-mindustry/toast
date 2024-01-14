c1b4bb138a13c2ae8add19dc12d57f016bf794fc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-15 00:21:22 +0700

    feat: Server enum display name

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/Server.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/Server.kt
@@ -1,5 +1,5 @@
 package com.github.kennarddh.mindustry.toast.core.commons
 
-enum class Server(val gameMode: GameMode) {
-    Survival(GameMode.Survival)
+enum class Server(val displayName: String, val gameMode: GameMode) {
+    Survival("Survival", GameMode.Survival)
 }
