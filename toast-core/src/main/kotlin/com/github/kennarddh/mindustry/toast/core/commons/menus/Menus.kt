67139d7a93363be685206dd140e12a0c187ede56
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-17 14:54:38 +0700

    feat: Check if menus is empty

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/menus/Menus.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/menus/Menus.kt
@@ -2,7 +2,15 @@ package com.github.kennarddh.mindustry.toast.core.commons.menus
 
 import mindustry.gen.Player
 
+class EmptyMenuException(message: String) : Exception(message)
+
 class Menus(private val menus: Map<String, Menu>) {
+    init {
+        if (menus.isEmpty()) {
+            throw EmptyMenuException("Menu must contains at least 1 menu")
+        }
+    }
+
     suspend fun open(player: Player): Map<String, String?> {
         val output: MutableMap<String, String?> = mutableMapOf()
 
