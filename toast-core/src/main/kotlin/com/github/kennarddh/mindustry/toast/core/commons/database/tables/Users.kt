18b96c060972f870e32e3f3a4d5decafce1cc6a8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-17 18:39:29 +0700

    feat: Change user's password length to 64 in table

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/Users.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/database/tables/Users.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Users : IntIdTable() {
     val username = varchar("username", 50)
-    val password = varchar("password", 255)
+    val password = varchar("password", 64)
     val role = enumerationByName<UserRole>("role", 100)
     val discordID = varchar("discordID", 255).nullable()
 }
