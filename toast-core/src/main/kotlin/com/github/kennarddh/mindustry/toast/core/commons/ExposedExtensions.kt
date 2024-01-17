75b6c9f14f97c710e412930481b54e4a6570bb2a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-17 14:04:56 +0700

    feat: Use non deprecated feature for exposed extensions

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/ExposedExtensions.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/ExposedExtensions.kt
@@ -3,7 +3,7 @@ package com.github.kennarddh.mindustry.toast.core.commons
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.api.ExposedBlob
 
-fun FieldSet.exists(where: SqlExpressionBuilder.() -> Op<Boolean>): Boolean = !select(where).empty()
+fun FieldSet.exists(where: SqlExpressionBuilder.() -> Op<Boolean>): Boolean = !selectAll().where(where).empty()
 
 fun Table.mediumblob(name: String): Column<ExposedBlob> =
     registerColumn(name, MediumBlobColumnType())
