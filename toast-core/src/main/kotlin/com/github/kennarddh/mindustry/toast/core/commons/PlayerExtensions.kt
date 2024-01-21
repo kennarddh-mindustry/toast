96eba4d66b4a0cb39844b7908191a2042668cc0b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-21 17:06:56 +0700

    feat: Info popup player extension

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/PlayerExtensions.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/PlayerExtensions.kt
@@ -6,3 +6,7 @@ import mindustry.gen.Player
 fun Player.infoMessage(message: String) {
     Call.infoMessage(this.con, message)
 }
+
+fun Player.infoPopup(message: String, duration: Float, align: Int, top: Int, left: Int, bottom: Int, right: Int) {
+    Call.infoPopup(this.con, message, duration, align, top, left, bottom, right)
+}
