7d734863b7375476b128a4154e4ac4ff79b3adc9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-24 00:24:32 +0700

    feat: Publish game events

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/Toast.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/Toast.kt
@@ -3,6 +3,7 @@ package com.github.kennarddh.mindustry.toast.core
 import arc.util.Log
 import com.github.kennarddh.mindustry.toast.core.commons.database.DatabaseHandler
 import com.github.kennarddh.mindustry.toast.core.commons.database.DatabaseSettings
+import com.github.kennarddh.mindustry.toast.core.handlers.GameEventsHandler
 import com.github.kennarddh.mindustry.toast.core.handlers.UserAccountHandler
 import com.github.kennarddh.mindustry.toast.core.handlers.UserStatsHandler
 import kennarddh.genesis.core.Genesis
@@ -16,6 +17,7 @@ class Toast : AbstractPlugin() {
 
         Genesis.addHandler(UserAccountHandler())
         Genesis.addHandler(UserStatsHandler())
+        Genesis.addHandler(GameEventsHandler())
 
         Log.info("[ToastCore] Loaded")
     }
---
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/GameEventsHandler.kt
@@ -0,0 +1,25 @@
+package com.github.kennarddh.mindustry.toast.core.handlers
+
+import com.github.kennarddh.mindustry.toast.core.commons.messaging.Messenger
+import com.github.kennarddh.mindustry.toast.core.commons.messaging.messages.GameEvent
+import com.github.kennarddh.mindustry.toast.core.commons.messaging.messages.PlayerJoinGameEvent
+import com.github.kennarddh.mindustry.toast.core.commons.messaging.messages.PlayerLeaveGameEvent
+import kennarddh.genesis.core.events.annotations.EventHandler
+import kennarddh.genesis.core.handlers.Handler
+import mindustry.game.EventType
+
+class GameEventsHandler : Handler() {
+    @EventHandler
+    fun onPlayerJoin(event: EventType.PlayerJoin) {
+        val player = event.player
+
+        Messenger.publishGameEvent(GameEvent(PlayerJoinGameEvent(player.name, player.uuid())))
+    }
+
+    @EventHandler
+    fun onPlayerLeave(event: EventType.PlayerLeave) {
+        val player = event.player
+        
+        Messenger.publishGameEvent(GameEvent(PlayerLeaveGameEvent(player.name, player.uuid())))
+    }
+}
