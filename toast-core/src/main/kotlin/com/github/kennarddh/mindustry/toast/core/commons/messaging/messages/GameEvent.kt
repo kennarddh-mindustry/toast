efdf2268b81a91f181d2f67da1f041f4de2f2a07
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-23 23:45:58 +0700

    feat: Game event

---
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/messaging/messages/GameEvent.kt
@@ -0,0 +1,23 @@
+package com.github.kennarddh.mindustry.toast.core.commons.messaging.messages
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonClassDiscriminator("type")
+sealed class GameEventBase
+
+@Serializable
+@SerialName("PlayerJoin")
+data class PlayerJoinGameEvent(val playerMindustryName: String, val playerMindustryUUID: String) : GameEventBase()
+
+@Serializable
+@SerialName("PlayerLeave")
+data class PlayerLeaveGameEvent(val playerMindustryName: String, val playerMindustryUUID: String) : GameEventBase()
+
+@Serializable
+data class GameEvent(val data: GameEventBase)
+
