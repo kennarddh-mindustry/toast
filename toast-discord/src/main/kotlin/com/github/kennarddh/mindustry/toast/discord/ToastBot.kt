559ec9c18a955d56e198efe4e45185122f986508
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-24 15:57:42 +0700

    feat: Toast bot

--- toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
+++ toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
@@ -1,5 +1,22 @@
 package com.github.kennarddh.mindustry.toast.discord
 
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+lateinit var jda: JDA
+
+class ReadyListener : ListenerAdapter() {
+    override fun onReady(event: ReadyEvent) {
+        println("[ToastDiscord] Bot Ready")
+    }
+}
+
 fun main() {
     println("[ToastDiscord] Loaded")
+
+    jda = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
+        .addEventListeners(ReadyListener())
+        .build()
 }
