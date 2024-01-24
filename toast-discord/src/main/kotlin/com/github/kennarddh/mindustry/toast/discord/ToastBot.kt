ef7d87a3ea7c0b4c644ceba0fe95c227ca254b9a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-25 01:39:39 +0700

    feat(discord): Enable intents

--- toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
+++ toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.requests.GatewayIntent
 
 lateinit var jda: JDA
 
@@ -20,5 +21,12 @@ fun main() {
     jda = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
         .setActivity(Activity.playing("Toast Mindustry Server"))
         .addEventListeners(ReadyListener())
+        .enableIntents(
+            GatewayIntent.GUILD_MESSAGES,
+            GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+            GatewayIntent.GUILD_MEMBERS,
+            GatewayIntent.GUILD_PRESENCES,
+            GatewayIntent.GUILD_MESSAGE_REACTIONS
+        )
         .build()
 }
