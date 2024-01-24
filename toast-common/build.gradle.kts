6186963dd74bfc999232e8e0cf3429cefd7b1814
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-24 13:31:01 +0700

    chore: Common build gradle file

---
+++ toast-common/build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization") version "1.9.10"
+    java
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://maven.xpdustry.com/releases")
+
+    maven {
+        url = uri("http://23.95.107.12:9999/releases")
+        isAllowInsecureProtocol = true
+    }
+}
+
+project.group = "com.github.kennarddh.mindustry"
+project.version = "1.0.0"
+
+val exposedVersion: String = "0.46.0"
+
+dependencies {
+    compileOnly("com.xpdustry:kotlin-runtime:3.1.0-k.1.9.10")
+
+    implementation(platform("org.jetbrains.exposed:exposed-bom:0.46.0"))
+    implementation("org.jetbrains.exposed:exposed-java-time")
+    implementation("org.jetbrains.exposed:exposed-core")
+    implementation("org.jetbrains.exposed:exposed-jdbc")
+
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+
+    implementation("com.password4j:password4j:1.7.3")
+
+    implementation("com.rabbitmq:amqp-client:5.20.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+sourceSets {
+    main {
+        java.srcDir("src/main/kotlin")
+    }
+}
+
+configurations.runtimeClasspath {
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-common")
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+    exclude("org.jetbrains.kotlin", "kotlin-reflect")
+    exclude("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
+    exclude("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8")
+    exclude("org.slf4j")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf("Multi-Release" to "true"))
+        }
+
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        from(sourceSets.main.get().output)
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "reposilite"
+            url = uri("http://23.95.107.12:9999/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupId
+            artifactId = "toast-common"
+            version = version
+            from(components["java"])
+        }
+    }
+}
+
+tasks.register("getArtifactPath") {
+    doLast { println(tasks.jar.get().archiveFile.get().toString()) }
+}
