c69017ffdaa33fe77f3b4b64e7db0d46498a269c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-25 01:43:43 +0700

    feat(discord): Get toast mindustry guild on bot ready

--- toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
+++ toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/ToastBot.kt
@@ -3,15 +3,22 @@ package com.github.kennarddh.mindustry.toast.discord
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.GatewayIntent
 
 lateinit var jda: JDA
 
+const val TOAST_MINDUSTRY_GUILD_ID = 1189553927843237888L
+
 class ReadyListener : ListenerAdapter() {
+    private lateinit var toastMindustryGuild: Guild
+
     override fun onReady(event: ReadyEvent) {
         println("[ToastDiscord] Bot Ready")
+
+        toastMindustryGuild = jda.getGuildById(TOAST_MINDUSTRY_GUILD_ID)!!
     }
 }
 
