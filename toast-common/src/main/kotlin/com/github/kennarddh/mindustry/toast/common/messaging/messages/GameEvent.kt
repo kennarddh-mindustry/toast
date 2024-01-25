a8dc94f6405b155f59b5f0deb4ead290612e7db2
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-25 17:08:32 +0700

    feat: More game events

--- toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/messaging/messages/GameEvent.kt
+++ toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/messaging/messages/GameEvent.kt
@@ -13,15 +13,22 @@ sealed class GameEventBase
 
 @Serializable
 @SerialName("PlayerJoin")
-data class PlayerJoinGameEvent(val playerMindustryName: String, val playerMindustryUUID: String) : GameEventBase()
+data class PlayerJoinGameEvent(val time: Long, val playerMindustryName: String, val playerMindustryUUID: String) :
+    GameEventBase()
 
 @Serializable
 @SerialName("PlayerLeave")
-data class PlayerLeaveGameEvent(val playerMindustryName: String, val playerMindustryUUID: String) : GameEventBase()
+data class PlayerLeaveGameEvent(val time: Long, val playerMindustryName: String, val playerMindustryUUID: String) :
+    GameEventBase()
 
 @Serializable
 @SerialName("PlayerChat")
-data class PlayerChatGameEvent(val playerMindustryName: String, val playerMindustryUUID: String, val message: String) :
+data class PlayerChatGameEvent(
+    val time: Long,
+    val playerMindustryName: String,
+    val playerMindustryUUID: String,
+    val message: String
+) :
     GameEventBase()
 
 // TODO: Implement map in database first. MapStart, MapEnd.
@@ -30,6 +37,18 @@ data class PlayerChatGameEvent(val playerMindustryName: String, val playerMindus
 //data class MapStartGameEvent() : GameEventBase()
 
 // TODO: Server start, server stop, server restart events.
+
+@Serializable
+@SerialName("ServerStart")
+data class ServerStartGameEvent(val time: Long) : GameEventBase()
+
+@Serializable
+@SerialName("ServerStop")
+data class ServerStopGameEvent(val time: Long) : GameEventBase()
+
+@Serializable
+@SerialName("ServerRestart")
+data class ServerRestartGameEvent(val time: Long) : GameEventBase()
 
 @Serializable
 data class GameEvent(val server: Server, val data: GameEventBase)
