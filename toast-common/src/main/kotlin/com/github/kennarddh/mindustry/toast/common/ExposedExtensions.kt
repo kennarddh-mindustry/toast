8bbee33a2974cb0c90c654e2b2ff036acd3add45
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-03 15:59:10 +0700

    feat(common): Insert if not exist and get exposed extension

--- toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/ExposedExtensions.kt
+++ toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/ExposedExtensions.kt
@@ -1,11 +1,17 @@
 package com.github.kennarddh.mindustry.toast.common
 
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.statements.api.ExposedBlob
 
 fun FieldSet.exists(where: SqlExpressionBuilder.() -> Op<Boolean>): Boolean = !selectAll().where(where).empty()
 fun FieldSet.selectOne(where: SqlExpressionBuilder.() -> Op<Boolean>): ResultRow? =
     selectAll().where(where).firstOrNull()
+
+fun <T : Table> T.insertIfNotExistAndGet(
+    where: SqlExpressionBuilder.() -> Op<Boolean>,
+    body: T.(InsertStatement<Number>) -> Unit
+): ResultRow = selectOne(where) ?: insert(body).resultedValues!!.first()
 
 fun Table.mediumblob(name: String): Column<ExposedBlob> =
     registerColumn(name, MediumBlobColumnType())
