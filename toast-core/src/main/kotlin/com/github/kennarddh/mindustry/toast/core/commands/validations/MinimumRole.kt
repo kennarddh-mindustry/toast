b25874806bfd388e9d488aa0e9040a3265f8a8ba
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-05 12:09:55 +0700

    fix: Minimum role command validation transaction

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commands/validations/MinimumRole.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commands/validations/MinimumRole.kt
@@ -2,41 +2,46 @@ package com.github.kennarddh.mindustry.toast.core.commands.validations
 
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.validations.CommandValidation
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.validations.CommandValidationDescription
+import com.github.kennarddh.mindustry.toast.common.CoroutineScopes
 import com.github.kennarddh.mindustry.toast.common.UserRole
 import com.github.kennarddh.mindustry.toast.common.database.tables.MindustryUser
 import com.github.kennarddh.mindustry.toast.common.database.tables.MindustryUserServerData
 import com.github.kennarddh.mindustry.toast.common.database.tables.Users
 import com.github.kennarddh.mindustry.toast.common.selectOne
+import kotlinx.coroutines.runBlocking
 import mindustry.gen.Player
 import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 
-fun validateMinimumRole(annotation: Annotation, player: Player?): Boolean {
+fun validateMinimumRole(annotation: Annotation, player: Player?): Boolean = runBlocking {
     if (player == null) {
         // On server console
-        return true
+        return@runBlocking true
     }
 
     val minimumRole = (annotation as MinimumRole).minimumRole
 
-    val user = Users.join(
-        MindustryUserServerData,
-        JoinType.INNER,
-        onColumn = Users.id,
-        otherColumn = MindustryUserServerData.userID
-    ).join(
-        MindustryUser,
-        JoinType.INNER,
-        onColumn = MindustryUserServerData.mindustryUserID,
-        otherColumn = MindustryUser.id
-    ).selectOne { MindustryUser.mindustryUUID eq player.uuid() }
+    newSuspendedTransaction(CoroutineScopes.IO.coroutineContext) {
+        val user = Users.join(
+            MindustryUserServerData,
+            JoinType.INNER,
+            onColumn = Users.id,
+            otherColumn = MindustryUserServerData.userID
+        ).join(
+            MindustryUser,
+            JoinType.INNER,
+            onColumn = MindustryUserServerData.mindustryUserID,
+            otherColumn = MindustryUser.id
+        ).selectOne { MindustryUser.mindustryUUID eq player.uuid() }
 
-    // Non registered user
-    if (user == null) return false
+        // Non registered user
+        if (user == null) return@newSuspendedTransaction false
 
-    val userRole = user[Users.role]
+        val userRole = user[Users.role]
 
-    return userRole >= minimumRole
+        return@newSuspendedTransaction userRole >= minimumRole
+    }
 }
 
 @Target(AnnotationTarget.FUNCTION)
