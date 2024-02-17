2b93e210f0026e2b7d00b96e6d7361cf48c0e5b8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-18 00:04:53 +0700

    chore: Bump genesis

--- toast-core/build.gradle.kts
+++ toast-core/build.gradle.kts
@@ -41,7 +41,7 @@ val metadata = ModMetadata.fromJson(project.file("plugin.json"))
 project.group = "com.github.kennarddh.mindustry"
 project.version = metadata.version
 
-val genesisVersion = "3.0.0-beta.4"
+val genesisVersion = "3.0.0-beta.5"
 
 dependencies {
     mindustryDependencies()
