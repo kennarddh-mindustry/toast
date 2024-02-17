24dea8b7feb7cef396d21cb0d32216db070dfce8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-17 14:28:31 +0700

    feat: Publish kick event

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserModerationHandler.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserModerationHandler.kt
@@ -12,15 +12,19 @@ import com.github.kennarddh.mindustry.toast.common.UserRole
 import com.github.kennarddh.mindustry.toast.common.database.tables.MindustryUser
 import com.github.kennarddh.mindustry.toast.common.database.tables.UserPunishments
 import com.github.kennarddh.mindustry.toast.common.database.tables.Users
+import com.github.kennarddh.mindustry.toast.common.messaging.Messenger
+import com.github.kennarddh.mindustry.toast.common.messaging.messages.GameEvent
+import com.github.kennarddh.mindustry.toast.common.messaging.messages.PlayerKickedGameEvent
 import com.github.kennarddh.mindustry.toast.core.commands.validations.MinimumRole
 import com.github.kennarddh.mindustry.toast.core.commons.Logger
+import com.github.kennarddh.mindustry.toast.core.commons.ToastVars
 import com.github.kennarddh.mindustry.toast.core.commons.getMindustryUser
 import com.github.kennarddh.mindustry.toast.core.commons.getUserAndMindustryUser
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import mindustry.gen.Player
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import kotlin.time.Duration
 
@@ -38,13 +42,13 @@ class UserModerationHandler : Handler() {
 
         target.kick("You were kicked for the reason:\n$reason")
 
-        newSuspendedTransaction(CoroutineScopes.IO.coroutineContext) {
+        val punishmentID = newSuspendedTransaction(CoroutineScopes.IO.coroutineContext) {
             val mindustryUser = target.getMindustryUser()!!
             val targetMindustryUser = target.getMindustryUser()!!
             val user = target.getUserAndMindustryUser()
             val targetUser = target.getUserAndMindustryUser()
 
-            UserPunishments.insert {
+            UserPunishments.insertAndGetId {
                 it[this.reason] = reason
                 it[this.endAt] =
                     Clock.System.now().plus(duration).toLocalDateTime(TimeZone.UTC)
@@ -60,6 +64,14 @@ class UserModerationHandler : Handler() {
                     it[this.targetUserID] = targetUser[Users.id]
             }
         }
+
+        Messenger.publishGameEvent(
+            GameEvent(
+                ToastVars.server,
+                Clock.System.now().toEpochMilliseconds(),
+                PlayerKickedGameEvent(punishmentID.value, target.name)
+            )
+        )
 
         CommandResult("Successfully kicked ${target.name}/${target.uuid()} for $duration with the reason \"$reason\"")
     }
