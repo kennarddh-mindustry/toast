6bb3abbcffb0f2b38a57f680b138e5143e26d840
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-18 15:53:59 +0700

    feat: Check role kick command

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserModerationHandler.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserModerationHandler.kt
@@ -5,6 +5,7 @@ import com.github.kennarddh.mindustry.genesis.core.commands.annotations.ClientSi
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.Command
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.ServerSide
 import com.github.kennarddh.mindustry.genesis.core.commands.result.CommandResult
+import com.github.kennarddh.mindustry.genesis.core.commands.result.CommandResultStatus
 import com.github.kennarddh.mindustry.genesis.core.handlers.Handler
 import com.github.kennarddh.mindustry.toast.common.CoroutineScopes
 import com.github.kennarddh.mindustry.toast.common.PunishmentType
@@ -20,6 +21,7 @@ import com.github.kennarddh.mindustry.toast.core.commons.Logger
 import com.github.kennarddh.mindustry.toast.core.commons.ToastVars
 import com.github.kennarddh.mindustry.toast.core.commons.getMindustryUser
 import com.github.kennarddh.mindustry.toast.core.commons.getUserAndMindustryUser
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -37,18 +39,24 @@ class UserModerationHandler : Handler() {
     @MinimumRole(UserRole.Mod)
     @ClientSide
     @ServerSide
-    suspend fun kick(player: Player? = null, target: Player, duration: Duration, reason: String) {
+    suspend fun kick(player: Player? = null, target: Player, duration: Duration, reason: String): CommandResult {
         Logger.info("${if (player == null) "Server" else player.name} kicked ${target.name}/${target.uuid()} for $duration with the reason \"$reason\"")
 
         target.kick("You were kicked for the reason:\n$reason")
 
-        val punishmentID = newSuspendedTransaction(CoroutineScopes.IO.coroutineContext) {
+        return newSuspendedTransaction(CoroutineScopes.IO.coroutineContext) {
             val mindustryUser = target.getMindustryUser()!!
             val targetMindustryUser = target.getMindustryUser()!!
-            val user = target.getUserAndMindustryUser()
+            val user = target.getUserAndMindustryUser()!!
             val targetUser = target.getUserAndMindustryUser()
 
-            UserPunishments.insertAndGetId {
+            if (targetUser != null && user[Users.role] <= targetUser[Users.role])
+                return@newSuspendedTransaction CommandResult(
+                    "Your role must be higher than target's role to change target's role.",
+                    CommandResultStatus.Failed
+                )
+
+            val punishmentID = UserPunishments.insertAndGetId {
                 it[this.reason] = reason
                 it[this.endAt] =
                     Clock.System.now().plus(duration).toLocalDateTime(TimeZone.UTC)
@@ -57,22 +65,23 @@ class UserModerationHandler : Handler() {
                 it[this.mindustryUserID] = mindustryUser[MindustryUser.id]
                 it[this.targetMindustryUserID] = targetMindustryUser[MindustryUser.id]
 
-                if (user != null)
-                    it[this.userID] = user[Users.id]
+                it[this.userID] = user[Users.id]
 
                 if (targetUser != null)
                     it[this.targetUserID] = targetUser[Users.id]
             }
+
+            CoroutineScopes.Main.launch {
+                Messenger.publishGameEvent(
+                    GameEvent(
+                        ToastVars.server,
+                        Clock.System.now().toEpochMilliseconds(),
+                        PlayerPunishedGameEvent(punishmentID.value, target.name)
+                    )
+                )
+            }
+
+            CommandResult("Successfully kicked ${target.name}/${target.uuid()} for $duration with the reason \"$reason\"")
         }
-
-        Messenger.publishGameEvent(
-            GameEvent(
-                ToastVars.server,
-                Clock.System.now().toEpochMilliseconds(),
-                PlayerPunishedGameEvent(punishmentID.value, target.name)
-            )
-        )
-
-        CommandResult("Successfully kicked ${target.name}/${target.uuid()} for $duration with the reason \"$reason\"")
     }
 }
