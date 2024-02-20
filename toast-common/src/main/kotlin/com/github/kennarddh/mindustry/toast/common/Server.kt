530f3924148e088e8edabcce5ab5a3244474d333
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-21 00:00:29 +0700

    feat: More servers

--- toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/Server.kt
+++ toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/Server.kt
@@ -6,5 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 enum class Server(val displayName: String, val gameMode: GameMode, val discordChannelID: Long) {
     @SerialName("Survival")
-    Survival("Survival", GameMode.Survival, 1199598512162213958L)
+    Survival("Survival", GameMode.Survival, 1199598512162213958L),
+
+    @SerialName("Attack")
+    Attack("Attack", GameMode.Attack, 1209544700189610025L),
+
+    @SerialName("PvP")
+    PvP("PvP", GameMode.PvP, 1209544729499275295L),
 }
