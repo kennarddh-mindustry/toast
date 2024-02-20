8eba58bae0bf9b6fa14d9e142861b52ca2d15fd8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-21 00:08:35 +0700

    feat: Lookup server enum by name

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/ToastVars.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/ToastVars.kt
@@ -4,8 +4,11 @@ import com.github.kennarddh.mindustry.toast.common.Server
 
 object ToastVars {
     val server: Server
-        get() = when (System.getenv("SERVER")) {
-            "Survival" -> Server.Survival
-            else -> throw Exception("${System.getenv("SERVER")} is not a valid server")
+        get() {
+            try {
+                return Server.valueOf(System.getenv("SERVER"))
+            } catch (error: IllegalArgumentException) {
+                throw IllegalArgumentException("${System.getenv("SERVER")} is not a valid server")
+            }
         }
 }
