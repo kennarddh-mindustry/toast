d96b39e760a880642b51a72fffbd54476dfd1bb3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-21 15:04:08 +0700

    feat(discord): Constants

---
+++ toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/DiscordConstant.kt
@@ -0,0 +1,7 @@
+package com.github.kennarddh.mindustry.toast.discord
+
+object DiscordConstant {
+    const val ServerListChannelID = 1208719661005279262L
+    const val NotificationsChannelID = 1208729130728947752L
+    const val ReportsChannelID = 1208728943356682240L
+}
