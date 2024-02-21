f8eb407c2c9b680a5733d5a334ee43070a2f5f0c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-21 15:44:40 +0700

    feat(discord): Update constants

--- toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/DiscordConstant.kt
+++ toast-discord/src/main/kotlin/com/github/kennarddh/mindustry/toast/discord/DiscordConstant.kt
@@ -1,7 +1,13 @@
 package com.github.kennarddh.mindustry.toast.discord
 
 object DiscordConstant {
-    const val ServerListChannelID = 1208719661005279262L
-    const val NotificationsChannelID = 1208729130728947752L
-    const val ReportsChannelID = 1208728943356682240L
+    const val TOAST_MINDUSTRY_GUILD_ID = 1189553927843237888L
+
+    const val SERVER_LIST_CHANNEL_ID = 1208719661005279262L
+
+    const val NOTIFICATIONS_CHANNEL_ID = 1208729130728947752L
+    const val REPORTS_CHANNEL_ID = 1208728943356682240L
+
+    const val PUNISHMENT_EMBED_COLOR = 0xff0000
+    const val PARDON_EMBED_COLOR = 0x00ff00
 }
