4c138d291ea86cbb08c75819ed6b4dd25d15022b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-22 13:52:14 +0700

    feat: Game mode and server rules in enum

--- toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/GameMode.kt
+++ toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/GameMode.kt
@@ -2,15 +2,22 @@ package com.github.kennarddh.mindustry.toast.common
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import mindustry.game.Rules
 
 @Serializable
-enum class GameMode {
+enum class GameMode(val applyRules: Rules.() -> Unit) {
     @SerialName("Survival")
-    Survival,
+    Survival({
+        buildSpeedMultiplier = 2f
+    }),
 
     @SerialName("Attack")
-    Attack,
+    Attack({
+        buildCostMultiplier = 0.5f
+    }),
 
     @SerialName("PvP")
-    PvP,
+    PvP({
+        buildSpeedMultiplier = 2f
+    }),
 }
--- toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/Server.kt
+++ toast-common/src/main/kotlin/com/github/kennarddh/mindustry/toast/common/Server.kt
@@ -2,15 +2,21 @@ package com.github.kennarddh.mindustry.toast.common
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import mindustry.game.Rules
 
 @Serializable
-enum class Server(val displayName: String, val gameMode: GameMode, val discordChannelID: Long) {
+enum class Server(
+    val displayName: String,
+    val gameMode: GameMode,
+    val discordChannelID: Long,
+    val applyRules: Rules.() -> Unit
+) {
     @SerialName("Survival")
-    Survival("Survival", GameMode.Survival, 1199598512162213958L),
+    Survival("Survival", GameMode.Survival, 1199598512162213958L, { }),
 
     @SerialName("Attack")
-    Attack("Attack", GameMode.Attack, 1209544700189610025L),
+    Attack("Attack", GameMode.Attack, 1209544700189610025L, { }),
 
     @SerialName("PvP")
-    PvP("PvP", GameMode.PvP, 1209544729499275295L),
+    PvP("PvP", GameMode.PvP, 1209544729499275295L, { }),
 }
