73b8c6a22ad4c82c538dba1d18d4aa3014e2af86
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-24 23:40:55 +0700

    feat: Get user player extension

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/PlayerExtensions.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/PlayerExtensions.kt
@@ -1,15 +1,32 @@
 package com.github.kennarddh.mindustry.toast.core.commons
 
+import com.github.kennarddh.mindustry.genesis.core.GenesisAPI
 import com.github.kennarddh.mindustry.toast.common.database.tables.MindustryUser
 import com.github.kennarddh.mindustry.toast.common.database.tables.MindustryUserServerData
 import com.github.kennarddh.mindustry.toast.common.database.tables.Users
 import com.github.kennarddh.mindustry.toast.common.selectOne
+import com.github.kennarddh.mindustry.toast.core.handlers.users.UserAccountHandler
 import mindustry.gen.Player
 import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.ResultRow
 
 val Player.mindustryServerUserDataWhereClause: Op<Boolean>
     get() = con.mindustryServerUserDataWhereClause
+
+/**
+ * Depends on UserAccountHandler stored user id
+ * Cannot be used before user is added to the users map
+ */
+fun Player.getUser(): ResultRow? {
+    val userID = GenesisAPI.getHandler<UserAccountHandler>()!!.users[this@getUser]!!.userID
+        ?: return null
+
+    return Users.selectOne {
+        Users.id eq userID
+    }
+}
+
 
 fun Player.getMindustryUserAndUserServerData() =
     MindustryUserServerData
