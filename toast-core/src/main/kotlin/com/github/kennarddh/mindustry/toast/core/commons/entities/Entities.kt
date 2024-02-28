877b72c181af87004c86fe709f9bbe669c9b5d0f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-28 23:43:16 +0700

    feat: Entities object

---
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/entities/Entities.kt
@@ -0,0 +1,8 @@
+package com.github.kennarddh.mindustry.toast.core.commons.entities
+
+import java.util.*
+
+
+object Entities {
+    val players: MutableList<PlayerData> = Collections.synchronizedList(mutableListOf())
+}
---
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/commons/entities/PlayerData.kt
@@ -0,0 +1,12 @@
+package com.github.kennarddh.mindustry.toast.core.commons.entities
+
+import com.github.kennarddh.mindustry.toast.common.UserRole
+import mindustry.gen.Player
+
+data class PlayerData(
+    var userID: Int?,
+    val mindustryUserID: Int,
+    val player: Player,
+    val originalName: String,
+    var role: UserRole
+)
