32270e9faf0c1d14f3418bde39d69da6e49ef41f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-29 12:54:39 +0700

    refactor: UserReportHandler

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserReportHandler.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/users/UserReportHandler.kt
@@ -1,18 +1,15 @@
 package com.github.kennarddh.mindustry.toast.core.handlers.users
 
-import arc.util.Strings
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.ClientSide
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.Command
 import com.github.kennarddh.mindustry.genesis.core.commands.annotations.Description
 import com.github.kennarddh.mindustry.genesis.core.commands.result.CommandResult
 import com.github.kennarddh.mindustry.genesis.core.handlers.Handler
-import com.github.kennarddh.mindustry.toast.common.database.Database
-import com.github.kennarddh.mindustry.toast.common.database.tables.Users
 import com.github.kennarddh.mindustry.toast.common.messaging.Messenger
 import com.github.kennarddh.mindustry.toast.common.messaging.messages.GameEvent
 import com.github.kennarddh.mindustry.toast.common.messaging.messages.PlayerReportedGameEvent
 import com.github.kennarddh.mindustry.toast.core.commons.ToastVars
-import com.github.kennarddh.mindustry.toast.core.commons.getUserOptionalAndMindustryUserAndUserServerData
+import com.github.kennarddh.mindustry.toast.core.commons.safeGetPlayerData
 import kotlinx.datetime.Clock
 import mindustry.gen.Player
 
@@ -20,25 +17,23 @@ class UserReportHandler : Handler() {
     @Command(["report"])
     @ClientSide
     @Description("Report a player.")
-    suspend fun kick(player: Player, target: Player, reason: String): CommandResult {
-        Database.newTransaction {
-            val user = player.getUserOptionalAndMindustryUserAndUserServerData()
-            val targetUser = player.getUserOptionalAndMindustryUserAndUserServerData()
+    suspend fun report(player: Player, target: Player, reason: String): CommandResult? {
+        val playerData = player.safeGetPlayerData() ?: return null
+        val targetPlayerData = player.safeGetPlayerData() ?: return null
 
-            Messenger.publishGameEvent(
-                "${ToastVars.server.name}.report",
-                GameEvent(
-                    ToastVars.server, Clock.System.now(),
-                    PlayerReportedGameEvent(
-                        Strings.stripColors(player.name),
-                        user?.get(Users.id)?.value,
-                        Strings.stripColors(target.name),
-                        targetUser?.get(Users.id)?.value,
-                        reason
-                    )
+        Messenger.publishGameEvent(
+            "${ToastVars.server.name}.report",
+            GameEvent(
+                ToastVars.server, Clock.System.now(),
+                PlayerReportedGameEvent(
+                    player.plainName(),
+                    playerData.userID,
+                    target.plainName(),
+                    targetPlayerData.userID,
+                    reason
                 )
             )
-        }
+        )
 
         return CommandResult("Successfully reported ${target.name} with the reason ${reason}.")
     }
