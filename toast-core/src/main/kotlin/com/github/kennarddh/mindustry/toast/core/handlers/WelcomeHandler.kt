aa31baf3cfb5e12a8b2c30b73ac33a4423c776cc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-03-17 13:36:07 +0700

    feat: Change motd

--- toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/WelcomeHandler.kt
+++ toast-core/src/main/kotlin/com/github/kennarddh/mindustry/toast/core/handlers/WelcomeHandler.kt
@@ -6,6 +6,6 @@ import mindustry.net.Administration.Config
 
 class WelcomeHandler : Handler {
     override suspend fun onInit() {
-        Config.motd.set("Welcome to | [red]Toast [orange]${ToastVars.server.displayName} [white]| [yellow]Owner: Bread | [white]Please join our discord by using [accent]/discord, [white]we are looking for new members and staff.")
+        Config.motd.set("Welcome to | [red]Toast [orange]${ToastVars.server.displayName} [white]| [yellow]Owner: Bread [white]| Please join our discord by using [accent]/discord, [white]we are looking for new members and staff.")
     }
 }
